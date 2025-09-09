c08eeb00268ca9a3881790591ae04618962438e2
darshit <darshitbhanderi2316@gmail.com>
2025-09-09 07:42:20 +0530

    one big resolved

--- Backend/seeds/003_seed_roles.js
+++ Backend/seeds/003_seed_roles.js
@@ -4,7 +4,7 @@
  */
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  a/* wait knex("roles").del(); */
+ /*  await knex("roles").del(); */
 
   // Inserts seed entries
   await knex("roles").insert([
