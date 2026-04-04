d11a9a89e35bf257554e5d8e81cddd9d0d8eda28
Darshit Bhanderi <darshitbhanderi2316@gmail.com>
2026-04-04 13:29:24 +0530

    bugfix

--- Frontend/src/routes/AppRoutes.js
+++ Frontend/src/routes/AppRoutes.js
@@ -6,7 +6,7 @@ import ProtectedRoute from "./ProtectedRoute";
 import Login from "../pages/auth/Login";
 import OTPPage from "../pages/auth/OTPPage";
 import Dashboard from "../pages/dashboard/dashboard";
-import RootLayoutGate from "../layout/RootLayoutGate";
+import RootLayoutGate from "./RootLayoutGate";
 import Orders from "../pages/orders/Orders";
 import OrderHistory from "../pages/orders/order-history/OrderHistory";
 import FieldVerifier from "../pages/field-verifier/FieldVerifier";
--- Frontend/src/routes/RootLayoutGate.js
+++ Frontend/src/routes/RootLayoutGate.js
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
-import Layout from "./Layout";
+import Layout from "../layout/Layout";
 
 /**
  * Root route behavior:
